b379eb0c18f495f0d6080e2ebf69e50ca835a450
kranthikandi <kranthikumar.kandi@gmail.com>
2021-01-01 06:07:49 -0800

    v2.01

--- js/app.js
+++ js/app.js
@@ -76,4 +76,13 @@ $(document).ready(function () {
         tl.progress();
         console.log(tl.progress());
     }
+    var svg = document.getElementsByTagName("svg");
+
+    var rect = document.getElementsByTagName("rect");
+
+    var text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    text.setAttribute('x', '10');
+    text.setAttribute('y', '20');
+    text.setAttribute('fill', '#000');
+    text.textContent = 'Hello, I am a blah blah blah blah blah';
 })
